a1d98e43eacad983ddfb268589a5c540b49f4f06
Brian McClune <bpmcclune@gmail.com>
2020-08-19 09:47:59 -0400

    made janken into a proper distributable package

---
+++ setup.py
@@ -0,0 +1,40 @@
+"""Janken setup script."""
+
+from setuptools import setup
+
+
+def get_version() -> str:
+    """Returns the current version of Janken.
+
+    Returns:
+        The current version string for Janken.
+
+    """
+
+    return '0.0.1'
+
+
+setup(
+    name='janken',
+    version=get_version(),
+    author='Brian McClune',
+    author_email='brian.mcclune@unnpp.gov',
+    description='A rock-paper-scissors image classifier!',
+    license='BSD',
+    keywords="rock paper scissors guu paa choki janken neural networks",
+    packages=['janken'],
+    entry_points={'console_scripts': ['janken = janken.__main__:main']},
+    include_package_data=True,
+    install_requires=[
+        'pandas',
+        'scipy==1.4.1',
+        'tensorboard==2.1.0',
+        'tensorflow==2.1.0',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python 3',
+        'Programming Language :: Python 3.7',
+    ],
+)
