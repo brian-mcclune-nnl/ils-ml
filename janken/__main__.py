ed45bf6169e249b0a334bb9c49f6543adfc97acd
Brian McClune <bpmcclune@gmail.com>
2020-08-19 08:42:26 -0400

    added argument parser to janken __main__

--- janken/__main__.py
+++ janken/__main__.py
@@ -2,9 +2,23 @@
 
 """
 
+import argparse
 import sys
 
 from janken import predict
+
+
+def get_parser() -> argparse.ArgumentParser:
+    """Returns a parser for Janken!
+
+    Returns:
+        Argument parser for Janken.
+
+    """
+
+    parser = argparse.ArgumentParser(description='Interpret Janken images.')
+    parser.add_argument('images', nargs='+', help='image files to interpret')
+    return parser
 
 
 def main():
@@ -12,7 +26,9 @@ def main():
 
     """
 
-    predict(sys.argv[1:])
+    parser = get_parser()
+    args = parser.parse_args()
+    predict(args.images)
 
 
 if __name__ == '__main__':
